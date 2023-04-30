2f5dbd8b0fb3187316f01de7c059543f2e0ebbb8
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-04-30 20:54:10 +0100

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,6 +2,7 @@
 """simple pagination"""
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """returns a tuple of index range to be returned
     from a list based on page and page_size
