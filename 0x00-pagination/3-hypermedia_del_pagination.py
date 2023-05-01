54257741fb80e76238d02f170a3d34998df85d8a
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-05-01 19:11:26 +0100

    task 3: deletion resistant hypermedia pagination

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import Dict, List, Tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: row for i, row in enumerate(dataset)
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """deletion-resistant pagination"""
+        len_dataset = len(self.indexed_dataset())
+        assert isinstance(index, int) and index < len_dataset
+
+        end = index + page_size
+        data = [row for i, row in self.indexed_dataset().items()
+                if i in range(index, end)]
+        return {
+            'index': index,
+            'next_index': end,
+            'page_size': page_size,
+            'data': data
+        }
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """returns a tuple of index range to be returned
+    from a list based on page and page_size
+    """
+    return (page - 1) * page_size, page * page_size
